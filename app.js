a36b6c7b0def487c2f393a7e0fd7dbdd2514444d
ad3rinto <aad3rinto@gmail.com>
2022-09-22 17:38:54 +0100

    add get routes to all pages

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
 
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+
+
+
 
 app.listen(3000, function(){
     console.log("Server started on port 3000")
