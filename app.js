473c26fde1dbdcf0ec7a54c4d133011f01e7dd11
ad3rinto <aad3rinto@gmail.com>
2022-09-23 13:08:18 +0100

    add user check and post route to login

--- app.js
+++ app.js
@@ -48,6 +48,22 @@ app.get("/login", function (req, res) {
     res.render("login");
 });
 
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({email: username}, function(err, foundUser){
+        if(err){
+            console.log(err)
+        } else {
+            if(foundUser){
+                if(foundUser.password === password){
+                    res.render("secrets")
+                }
+            }
+        }
+    })
+});
+
 
 
 
